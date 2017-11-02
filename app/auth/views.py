d7cd8c2a3498e502e865689ca7ec08be769a1fa9
suyashkumar <suyashkumar2409@gmail.com>
2017-11-02 11:44:35 +0000

    Login, registration and related mailing set up

--- app/auth/views.py
+++ app/auth/views.py
@@ -3,6 +3,7 @@ from . import auth
 from flask_login import login_required, login_user, logout_user
 from ..models import login_user
 from .forms import LoginForm
+from ..email import send_email
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -30,7 +31,43 @@ def register():
 	if form.validate_on_submit():
 		user = User(email=form.email.data, username=form.username.data, password=form.password.data)
 		db.session.add(user)
+		db.session.commit()
 
-		flash('You can now login.')
+		token = user.generate_confirmation_token()
+		send_email(user.email, 'Confirm Your Account', 'auth/email/confirm', user = user, token =token)
+
+
+		flash('A confirmation email has been sent to your email ID.')
 		return redirect(url_for('auth.login'))
 	return render_template('auth/register.html', form=form)
+
+@auth.route('/confirm/<token>')
+@login_required
+def confirm(token):
+	if current_user.confirmed:
+		return redirect(url_for('main.index'))
+	else:
+		if current_user.confirm(token):
+			flash('Your account has been confirmed!')
+		else:
+			flash('The confirmation link is invalid or has expired!')
+		return redirect(url_for('main.index'))
+
+@auth.before_app_request
+def before_request():
+	if current_user.is_authenticated() and not current_user.confirmed and request.endpoint[:5] != 'auth.':
+		return redirect(url_for('auth.unconfirmed'))
+
+@auth.route('/unconfirmed')
+def unconfirmed():
+	if current_user.is_anonymous() or current_user.confirmed:
+		return redirect('main.index')
+	return render_template('auth/unconfirmed.html')
+
+@auth.route('/confirm')
+@login_required
+def resend_confirmation():
+	token = current_user.generate_confirmation_token()
+	send_email('auth/email/confirm', 'Confirm Your Account', user = user, token = token)
+	flash('A new confirmation email has been sent to you by email.')
+	return redirect(url_for('main.index'))
--- app/models.py
+++ app/models.py
@@ -2,6 +2,8 @@ from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from . import login_manager
+from flask import current_app
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -20,7 +22,26 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
+    confirmed = db.Column(db.Boolean, default = False)
 
+    def generate_confirmation_token(self, expiration = 60*60*48):
+    	s = Serializer(current_app.config['SECRET_KEY'], expiration)
+    	return s.dumps({'confirm':self.id})
+
+    def confirm(self, token):
+    	s = Serializer(current_app.config['SECRET_KEY'])
+    	try:
+    		data = s.loads(token)
+    	except:
+    		return False
+    	if data.get('confirm') != self.id:
+    		return False
+
+ 	   	self.confirmed = true
+ 	   	db.session.add(self)
+ 	   	return True
+
+ 	   	
     @property
     def password(self):
         raise AttributeError('password is not a readable attribute')
@@ -31,7 +52,7 @@ class User(UserMixin, db.Model):
 
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
-        
+
     def __repr__(self):
         return '<User %r>' % self.username
 
