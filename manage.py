6105a3c95ae0227ec2d956d1940b1eeb2902dc8f
suyash <suyashkumar2409@gmail.com>
2017-11-08 01:42:56 +0530

    host changed

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 import os
 from app import create_app, db
 from app.models import User, Role, Request, Quiz, Score
-from flask_script import Manager, Shell
+from flask_script import Manager, Shell, Server
 from flask_migrate import Migrate, MigrateCommand
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
@@ -14,6 +14,7 @@ def make_shell_context():
     return dict(app=app, db=db, User=User, Role=Role, Request = Request, Quiz = Quiz, Score = Score)
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
+manager.add_command('runserver', Server(host="0.0.0.0", port=9000))
 
 
 @manager.command
